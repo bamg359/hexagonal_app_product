97187ad05a4107d78570187a3aa9c37706929282
Ber Mira <bamg359@gmail.com>
2025-03-23 22:38:09 -0500

    create project microservice product

---
+++ src/main/java/com/easycode/ddd_hex_product/DddHexProductApplication.java
@@ -0,0 +1,13 @@
+package com.easycode.ddd_hex_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddHexProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddHexProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easycode/ddd_hex_product/DddHexProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easycode.ddd_hex_product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddHexProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
